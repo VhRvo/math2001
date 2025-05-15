8ef0896b7d3598516442fc624ca8a89dd52cbc69
VhRvo <zyzhuo@outlook.com>
2025-05-15 20:04:02 +0800

    Finished one section.

--- Math2001/02_Proofs_with_Structure/02_Invoking_Lemmas.lean
+++ Math2001/02_Proofs_with_Structure/02_Invoking_Lemmas.lean
@@ -7,26 +7,47 @@ math2001_init
 example {x : ℚ} (hx : 3 * x = 2) : x ≠ 1 := by
   apply ne_of_lt
   calc
-    x = 3 * x / 3 := by ring
+    x
+    _ = 3 * x / 3 := by ring
     _ = 2 / 3 := by rw [hx]
     _ < 1 := by numbers
 
 example {y : ℝ} : y ^ 2 + 1 ≠ 0 := by
-  sorry
+  apply ne_of_gt
+  calc
+    (0 : ℝ)
+    _ < 1 := by numbers
+    _ ≤ y ^ 2 + 1 := by extra
 
 example {a b : ℝ} (h1 : a ^ 2 + b ^ 2 = 0) : a ^ 2 = 0 := by
   apply le_antisymm
-  calc
-    a ^ 2 ≤ a ^ 2 + b ^ 2 := by extra
-    _ = 0 := h1
-  extra
-
+  { calc
+    a ^ 2
+    _ ≤ a ^ 2 + b ^ 2 := by extra
+    _ = 0 := h1 }
+  { extra }
 
 /-! # Exercises -/
 
 
 example {m : ℤ} (hm : m + 1 = 5) : 3 * m ≠ 6 := by
-  sorry
+  have h : m = 4 := by addarith [hm]
+  apply ne_of_gt
+  calc
+    (6 : ℤ)
+    _ < 12 := by numbers
+    _ = 3 * 4 := by ring
+    _ = 3 * m := by rw [←h]
 
 example {s : ℚ} (h1 : 3 * s ≤ -6) (h2 : 2 * s ≥ -4) : s = -2 := by
-  sorry
+  apply le_antisymm
+  { calc
+    s
+    _ = 3 * s / 3 := by ring
+    _ ≤ -6 / 3 := by rel [h1]
+    _ = -2 := by ring }
+  { calc
+    (-2 : ℚ)
+    _ = -4 / 2 := by ring
+    _ ≤ 2 * s / 2 := by rel [h2]
+    _ = s := by ring }
