ce68698144c446631610b99d5c95e34b281ff98e
VhRvo <zyzhuo@outlook.com>
2025-05-19 23:17:34 +0800

    Finshed one half of Exists section.

--- Math2001/02_Proofs_with_Structure/05_Exists.lean
+++ Math2001/02_Proofs_with_Structure/05_Exists.lean
@@ -17,31 +17,49 @@ example {t : ℝ} (h : ∃ a : ℝ, a * t < 0) : t ≠ 0 := by
   obtain ⟨x, hxt⟩ := h
   have H := le_or_gt x 0
   obtain hx | hx := H
-  · have hxt' : 0 < (-x) * t := by addarith [hxt]
+  { have hxt' : 0 < (-x) * t := by addarith [hxt]
     have hx' : 0 ≤ -x := by addarith [hx]
     cancel -x at hxt'
     apply ne_of_gt
-    apply hxt'
-  · sorry
+    exact hxt' }
+  { have h1 : 0 < x * -t := by
+      calc
+        0
+        _ < - (x * t) := by addarith [hxt]
+        _ = x * -t := by ring
+    cancel x at h1
+    apply ne_of_lt
+    addarith [h1] }
 
 example : ∃ n : ℤ, 12 * n = 84 := by
   use 7
   numbers
 
-
 example (x : ℝ) : ∃ y : ℝ, y > x := by
   use x + 1
   extra
 
-
 example : ∃ m n : ℤ, m ^ 2 - n ^ 2 = 11 := by
-  sorry
+  use 6
+  use 5
+  ring
 
 example (a : ℤ) : ∃ m n : ℤ, m ^ 2 - n ^ 2 = 2 * a + 1 := by
-  sorry
+  use (a + 1)
+  use a
+  ring
 
 example {p q : ℝ} (h : p < q) : ∃ x, p < x ∧ x < q := by
-  sorry
+  use (p + q) / 2
+  constructor
+  { calc
+      p
+      _ = (p + p) / 2 := by ring
+      _ < (p + q) / 2 := by rel [h] }
+  { calc
+      (p + q) / 2
+      _ < (q + q) / 2 := by rel [h]
+      _ = q := by ring }
 
 example : ∃ a b c d : ℕ,
     a ^ 3 + b ^ 3 = 1729 ∧ c ^ 3 + d ^ 3 = 1729 ∧ a ≠ c ∧ a ≠ d := by
@@ -58,17 +76,59 @@ example : ∃ a b c d : ℕ,
 
 
 example : ∃ t : ℚ, t ^ 2 = 1.69 := by
-  sorry
+  use 1.3
+  ring
+
 example : ∃ m n : ℤ, m ^ 2 + n ^ 2 = 85 := by
-  sorry
+  use 6
+  use 7
+  ring
 
 example : ∃ x : ℝ, x < 0 ∧ x ^ 2 < 1 := by
-  sorry
+  use -0.5
+  constructor
+  { numbers }
+  { numbers }
+
 example : ∃ a b : ℕ, 2 ^ a = 5 * b + 1 := by
-  sorry
+  use 4
+  use 3
+  ring
 
 example (x : ℚ) : ∃ y : ℚ, y ^ 2 > x := by
-  sorry
+  obtain hx | hx := le_or_gt x 0
+  { use 1 - x
+    have h : 1 - x > 0 := by
+      calc
+        1 - x
+        _ >= 1 - 0 := by addarith [hx]
+        _ = 1 := by ring
+        _ > 0 := by numbers
+    calc
+      (1 - x) ^ 2
+      _ > 0 ^ 2 := by rel [h]
+      _ = 0 := by ring
+      _ ≥ x := by rel [hx]
+   }
+  { obtain hx' | hx' := le_or_gt x 1
+    { use 1 + x
+      have h : 1 + x > 1 := by
+        calc
+          1 + x
+          _ > 1 + 0 := by rel [hx]
+          _ = 1 := by ring
+      calc
+        (1 + x) ^ 2
+        _ > 1 ^ 2 := by rel [h]
+        _ = 1 := by ring
+        _ ≥ x := by rel [hx'] }
+    { use x
+      calc
+        x ^ 2
+        _ = x * x := by ring
+        _ > x * 1 := by rel [hx']
+        _ = x := by ring } }
+
 
 example {t : ℝ} (h : ∃ a : ℝ, a * t + 1 < a + t) : t ≠ 1 := by
   sorry
