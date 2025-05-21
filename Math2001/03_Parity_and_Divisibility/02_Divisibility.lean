4b471433d1d363bbb7c850e019094c5c25a4f0bd
VhRvo <zyzhuo@outlook.com>
2025-05-21 17:29:10 +0800

    Finished Divisibility Section.

--- Math2001/03_Parity_and_Divisibility/02_Divisibility.lean
+++ Math2001/03_Parity_and_Divisibility/02_Divisibility.lean
@@ -10,23 +10,36 @@ example : (11 : ℕ) ∣ 88 := by
   use 8
   numbers
 
-
 example : (-2 : ℤ) ∣ 6 := by
-  sorry
+  -- dsimp [Dvd.dvd]
+  dsimp [(· ∣ ·)]
+  -- dsimp [(. ∣ .)]
+  use -3
+  numbers
 
 example {a b : ℤ} (hab : a ∣ b) : a ∣ b ^ 2 + 2 * b := by
   obtain ⟨k, hk⟩ := hab
   use k * (a * k + 2)
-  calc
-    b ^ 2 + 2 * b = (a * k) ^ 2 + 2 * (a * k) := by rw [hk]
-    _ = a * (k * (a * k + 2)) := by ring
-
+  rw [hk]
+  ring
+  -- calc
+  --   b ^ 2 + 2 * b
+  --   _ = (a * k) ^ 2 + 2 * (a * k) := by rw [hk]
+  --   _ = a * (k * (a * k + 2)) := by ring
 
 example {a b c : ℕ} (hab : a ∣ b) (hbc : b ^ 2 ∣ c) : a ^ 2 ∣ c := by
-  sorry
+  dsimp [(· ∣ ·)] at *
+  obtain ⟨k₁, hk₁⟩ := hab
+  obtain ⟨k₂, hk₂⟩ := hbc
+  use k₁ ^ 2 * k₂
+  rw [hk₂, hk₁]
+  ring
 
 example {x y z : ℕ} (h : x * y ∣ z) : x ∣ z := by
-  sorry
+  obtain ⟨k, hk⟩ := h
+  use y * k
+  rw [hk]
+  ring
 
 example : ¬(5 : ℤ) ∣ 12 := by
   apply Int.not_dvd_of_exists_lt_and_lt
@@ -34,7 +47,6 @@ example : ¬(5 : ℤ) ∣ 12 := by
   constructor
   · numbers -- show `5 * 2 < 12`
   · numbers -- show `12 < 5 * (2 + 1)`
-
 
 example {a b : ℕ} (hb : 0 < b) (hab : a ∣ b) : a ≤ b := by
   obtain ⟨k, hk⟩ := hab
@@ -49,36 +61,73 @@ example {a b : ℕ} (hb : 0 < b) (hab : a ∣ b) : a ≤ b := by
     _ ≤ a * k := by rel [H]
     _ = b := by rw [hk]
 
-
 example {a b : ℕ} (hab : a ∣ b) (hb : 0 < b) : 0 < a := by
-  sorry
+  obtain ⟨k, hk⟩ := hab
+  have H1 :=
+    calc
+      0
+      _ < b := hb
+      _ = a * k := by rw [hk]
+  cancel k at H1
 
 /-! # Exercises -/
 
-
 example (t : ℤ) : t ∣ 0 := by
-  sorry
+  use 0
+  ring
 
 example : ¬(3 : ℤ) ∣ -10 := by
-  sorry
+  apply Int.not_dvd_of_exists_lt_and_lt
+  use -4
+  constructor
+  { numbers }
+  { numbers }
 
 example {x y : ℤ} (h : x ∣ y) : x ∣ 3 * y - 4 * y ^ 2 := by
-  sorry
+  obtain ⟨k, hk⟩ := h
+  use 3 * k - 4 * x * k ^ 2
+  rw [hk]
+  ring
 
 example {m n : ℤ} (h : m ∣ n) : m ∣ 2 * n ^ 3 + n := by
-  sorry
+  obtain ⟨k, hk⟩ := h
+  use 2 * m ^ 2 * k ^ 3 + k
+  rw [hk]
+  ring
 
 example {a b : ℤ} (hab : a ∣ b) : a ∣ 2 * b ^ 3 - b ^ 2 + 3 * b := by
-  sorry
+  obtain ⟨k, hk⟩ := hab
+  use 2 * a ^ 2 * k ^ 3 - a * k ^ 2 + 3 * k
+  rw [hk]
+  ring
 
 example {k l m : ℤ} (h1 : k ∣ l) (h2 : l ^ 3 ∣ m) : k ^ 3 ∣ m := by
-  sorry
+  obtain ⟨a, ha⟩ := h1
+  obtain ⟨b, hb⟩ := h2
+  use a ^ 3 * b
+  rw [hb, ha]
+  ring
 
 example {p q r : ℤ} (hpq : p ^ 3 ∣ q) (hqr : q ^ 2 ∣ r) : p ^ 6 ∣ r := by
-  sorry
+  obtain ⟨a, ha⟩ := hpq
+  obtain ⟨b, hb⟩ := hqr
+  use a ^ 2 * b
+  rw [hb, ha]
+  ring
 
 example : ∃ n : ℕ, 0 < n ∧ 9 ∣ 2 ^ n - 1 := by
-  sorry
+  use 6
+  constructor
+  { numbers }
+  { use 7
+    numbers }
 
 example : ∃ a b : ℤ, 0 < b ∧ b < a ∧ a - b ∣ a + b := by
-  sorry
+  use 3
+  use 1
+  constructor
+  { numbers }
+  { constructor
+    { numbers }
+    { use 2
+      numbers } }
