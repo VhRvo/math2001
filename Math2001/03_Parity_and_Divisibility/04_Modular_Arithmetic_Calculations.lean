fedc95bdce2b832ba0827449900e62ef0dce92d0
VhRvo <zyzhuo@outlook.com>
2025-05-22 17:16:28 +0800

    Finished Modular Arithmetic Calculations Section.

--- Math2001/03_Parity_and_Divisibility/04_Modular_Arithmetic_Calculations.lean
+++ Math2001/03_Parity_and_Divisibility/04_Modular_Arithmetic_Calculations.lean
@@ -11,56 +11,114 @@ example {a b : ℤ} (ha : a ≡ 2 [ZMOD 4]) :
 example {a b : ℤ} (ha : a ≡ 4 [ZMOD 5]) (hb : b ≡ 3 [ZMOD 5]) :
     a * b + b ^ 3 + 3 ≡ 2 [ZMOD 5] :=
   calc
-    a * b + b ^ 3 + 3 ≡ 4 * b + b ^ 3 + 3 [ZMOD 5] := by rel [ha]
+    a * b + b ^ 3 + 3
+    _ ≡ 4 * b + b ^ 3 + 3 [ZMOD 5] := by rel [ha]
     _ ≡ 4 * 3 + 3 ^ 3 + 3 [ZMOD 5] := by rel [hb]
     _ = 2 + 5 * 8 := by numbers
     _ ≡ 2 [ZMOD 5] := by extra
 
-
 example : ∃ a : ℤ, 6 * a ≡ 4 [ZMOD 11] := by
   use 8
   calc
-    (6:ℤ) * 8 = 4 + 4 * 11 := by numbers
+    (6 : ℤ) * 8
+    _ = 4 + 4 * 11 := by numbers
     _ ≡ 4 [ZMOD 11] := by extra
-
 
 example {x : ℤ} : x ^ 3 ≡ x [ZMOD 3] := by
   mod_cases hx : x % 3
   calc
-    x ^ 3 ≡ 0 ^ 3 [ZMOD 3] := by rel [hx]
+    x ^ 3
+    _ ≡ 0 ^ 3 [ZMOD 3] := by rel [hx]
     _ = 0 := by numbers
     _ ≡ x [ZMOD 3] := by rel [hx]
   calc
-    x ^ 3 ≡ 1 ^ 3 [ZMOD 3] := by rel [hx]
+    x ^ 3
+    _ ≡ 1 ^ 3 [ZMOD 3] := by rel [hx]
     _ = 1 := by numbers
     _ ≡ x [ZMOD 3] := by rel [hx]
   calc
-    x ^ 3 ≡ 2 ^ 3 [ZMOD 3] := by rel [hx]
+    x ^ 3
+    _ ≡ 2 ^ 3 [ZMOD 3] := by rel [hx]
     _ = 2 + 3 * 2 := by numbers
     _ ≡ 2 [ZMOD 3] := by extra
     _ ≡ x [ZMOD 3] := by rel [hx]
 
 /-! # Exercises -/
 
-
 example {n : ℤ} (hn : n ≡ 1 [ZMOD 3]) : n ^ 3 + 7 * n ≡ 2 [ZMOD 3] :=
-  sorry
+  calc
+    n ^ 3 + 7 * n
+    _ ≡ 1 ^ 3 + 7 * 1 [ZMOD 3] := by rel [hn]
+    _ = 2 + 3 * 2 := by ring
+    _ ≡ 2 [ZMOD 3] := by extra
 
 example {a : ℤ} (ha : a ≡ 3 [ZMOD 4]) :
     a ^ 3 + 4 * a ^ 2 + 2 ≡ 1 [ZMOD 4] :=
-  sorry
+  calc
+    a ^ 3 + 4 * a ^ 2 + 2
+    _ ≡ 3 ^ 3 + 4 * 3 ^ 2 + 2 [ZMOD 4] := by rel [ha]
+    _ = 1 + 4 * 16 := by ring
+    _ ≡ 1 [ZMOD 4] := by extra
 
-example (a b : ℤ) : (a + b) ^ 3 ≡ a ^ 3 + b ^ 3 [ZMOD 3] :=
-  sorry
+example (a b : ℤ) : (a + b) ^ 3 ≡ a ^ 3 + b ^ 3 [ZMOD 3] := by
+  -- dsimp [Int.ModEq]
+  use a ^ 2 * b + a * b ^ 2
+  ring
+  -- calc
+  --   (a + b) ^ 3 - (a ^ 3 + b ^ 3)
+  --   _ = 3 * a ^ 2 * b + 3 * a * b ^ 2 := by ring
+  --   _ = 3 * (a ^ 2 * b + a * b ^ 2) := by ring
 
 example : ∃ a : ℤ, 4 * a ≡ 1 [ZMOD 7] := by
-  sorry
+  use 2
+  use 1
+  ring
 
 example : ∃ k : ℤ, 5 * k ≡ 6 [ZMOD 8] := by
-  sorry
+  use 6
+  use 3
+  ring
 
 example (n : ℤ) : 5 * n ^ 2 + 3 * n + 7 ≡ 1 [ZMOD 2] := by
-  sorry
+  mod_cases hn : n % 2
+  { calc
+      5 * n ^ 2 + 3 * n + 7
+      _ ≡ 5 * 0 ^ 2 + 3 * 0 + 7 [ZMOD 2] := by rel [hn]
+      _ = 1 + 2 * 3 := by ring
+      _ ≡ 1 [ZMOD 2] := by extra }
+  { calc
+      5 * n ^ 2 + 3 * n + 7
+      _ ≡ 5 * 1 ^ 2 + 3 * 1 + 7 [ZMOD 2] := by rel [hn]
+      _ = 1 + 2 * 7 := by ring
+      _ ≡ 1 [ZMOD 2] := by extra }
 
 example {x : ℤ} : x ^ 5 ≡ x [ZMOD 5] := by
-  sorry
+  mod_cases hx : x % 5
+  { calc
+      x ^ 5
+      _ ≡ 0 ^ 5 [ZMOD 5] := by rel [hx]
+      _ = 0 := by ring
+      _ ≡ x [ZMOD 5] := by rel [hx] }
+  { calc
+      x ^ 5
+      _ ≡ 1 ^ 5 [ZMOD 5] := by rel [hx]
+      _ = 1 := by ring
+      _ ≡ x [ZMOD 5] := by rel [hx] }
+  { calc
+      x ^ 5
+      _ ≡ 2 ^ 5 [ZMOD 5] := by rel [hx]
+      _ = 2 + 5 * 6 := by ring
+      _ ≡ 2 [ZMOD 5] := by extra
+      _ ≡ x [ZMOD 5] := by rel [hx] }
+  { calc
+      x ^ 5
+      _ ≡ 3 ^ 5 [ZMOD 5] := by rel [hx]
+      _ = 3 + 5 * 48 := by ring
+      _ ≡ 3 [ZMOD 5] := by extra
+      _ ≡ x [ZMOD 5] := by rel [hx] }
+  { calc
+      x ^ 5
+      _ ≡ 4 ^ 5 [ZMOD 5] := by rel [hx]
+      _ = 4 + 5 * 204 := by ring
+      _ ≡ 4 [ZMOD 5] := by extra
+      _ ≡ x [ZMOD 5] := by rel [hx] }
